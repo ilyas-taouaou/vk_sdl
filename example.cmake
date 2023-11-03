59ef05d33c3ce504494557d51367788f3cbef5be
ilyas taouaou <146193882+ilyas-taouaou@users.noreply.github.com>
2023-11-03 07:48:02 +0100

    Create example.cmake

---
+++ example.cmake
@@ -0,0 +1,17 @@
+cmake_minimum_required(VERSION 3.26)
+project(example C)
+
+set(CMAKE_C_STANDARD 23)
+
+file(
+        DOWNLOAD
+        https://github.com/cpm-cmake/CPM.cmake/releases/download/v0.38.3/CPM.cmake
+        ${CMAKE_CURRENT_BINARY_DIR}/cmake/CPM.cmake
+        EXPECTED_HASH SHA256=cc155ce02e7945e7b8967ddfaff0b050e958a723ef7aad3766d368940cb15494
+)
+include(${CMAKE_CURRENT_BINARY_DIR}/cmake/CPM.cmake)
+
+CPMAddPackage("gh:ilyas-taouaou/vk_sdl#main")
+
+add_executable(${PROJECT_NAME} main.c)
+target_link_libraries(${PROJECT_NAME} PRIVATE vk_sdl)
